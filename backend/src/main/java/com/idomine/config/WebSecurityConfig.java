ece11b1dbf7551a7133cc0b7f3648e4508a2ed77
lindontavares lyndon.tavares@datapar.com.py <lyndon.tavares@datapar.com.py>
2018-06-15 17:55:08 -0300

    Adicionado CORS.

--- backend/src/main/java/com/idomine/config/WebSecurityConfig.java
+++ backend/src/main/java/com/idomine/config/WebSecurityConfig.java
@@ -1,5 +1,7 @@
 package com.idomine.config;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -15,6 +17,9 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.idomine.security.auth.AuthenticationFailureHandler;
 import com.idomine.security.auth.AuthenticationSuccessHandler;
@@ -29,64 +34,82 @@ import com.idomine.service.impl.CustomUserDetailsService;
 
 @Configuration
 @EnableGlobalMethodSecurity(prePostEnabled = true)
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter
+{
 
-  @Value("${jwt.cookie}")
-  private String TOKEN_COOKIE;
+    @Value("${jwt.cookie}")
+    private String TOKEN_COOKIE;
 
-  @Bean
-  public TokenAuthenticationFilter jwtAuthenticationTokenFilter() throws Exception {
-    return new TokenAuthenticationFilter();
-  }
+    @Bean
+    public TokenAuthenticationFilter jwtAuthenticationTokenFilter() throws Exception
+    {
+        return new TokenAuthenticationFilter();
+    }
 
-  @Bean
-  @Override
-  public AuthenticationManager authenticationManagerBean() throws Exception {
-    return super.authenticationManagerBean();
-  }
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception
+    {
+        return super.authenticationManagerBean();
+    }
 
-  @Bean
-  public PasswordEncoder passwordEncoder() {
-    return new BCryptPasswordEncoder();
-  }
+    @Bean
+    public PasswordEncoder passwordEncoder()
+    {
+        return new BCryptPasswordEncoder();
+    }
 
-  @Autowired
-  private CustomUserDetailsService jwtUserDetailsService;
+    @Autowired
+    private CustomUserDetailsService jwtUserDetailsService;
 
-  @Autowired
-  private RestAuthenticationEntryPoint restAuthenticationEntryPoint;
+    @Autowired
+    private RestAuthenticationEntryPoint restAuthenticationEntryPoint;
 
-  @Autowired
-  private LogoutSuccess logoutSuccess;
+    @Autowired
+    private LogoutSuccess logoutSuccess;
 
-  @Autowired
-  public void configureGlobal(AuthenticationManagerBuilder authenticationManagerBuilder)
-      throws Exception {
-    authenticationManagerBuilder.userDetailsService(jwtUserDetailsService)
-        .passwordEncoder(passwordEncoder());
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder authenticationManagerBuilder)
+            throws Exception
+    {
+        authenticationManagerBuilder.userDetailsService(jwtUserDetailsService)
+                .passwordEncoder(passwordEncoder());
 
-  }
+    }
 
-  @Autowired
-  private AuthenticationSuccessHandler authenticationSuccessHandler;
+    @Autowired
+    private AuthenticationSuccessHandler authenticationSuccessHandler;
 
-  @Autowired
-  private AuthenticationFailureHandler authenticationFailureHandler;
+    @Autowired
+    private AuthenticationFailureHandler authenticationFailureHandler;
 
-  @Override
-  protected void configure(HttpSecurity http) throws Exception {
-      http.cors().disable();
-    http.csrf().ignoringAntMatchers("/api/login", "/api/signup","api/report")
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+        configuration.setAllowCredentials(true);
+        configuration.setAllowedMethods(Arrays.asList("GET","POST","DELETE"));
+        configuration.setAllowedHeaders(Arrays.asList("Origin","x-requested-With", "content-type", "accept","Authorization"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+    
+    
+    @Override
+    protected void configure(HttpSecurity http) throws Exception
+    {
+        http.cors().configurationSource(  corsConfigurationSource());
         
-        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()).and()
-        .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
-        .exceptionHandling().authenticationEntryPoint(restAuthenticationEntryPoint).and()
-        .addFilterBefore(jwtAuthenticationTokenFilter(), BasicAuthenticationFilter.class)
-        .authorizeRequests().anyRequest().authenticated().and().formLogin().loginPage("/api/login")
-        .successHandler(authenticationSuccessHandler).failureHandler(authenticationFailureHandler)
-        .and().logout().logoutRequestMatcher(new AntPathRequestMatcher("/api/logout"))
-        .logoutSuccessHandler(logoutSuccess).deleteCookies(TOKEN_COOKIE);
+        http.csrf().ignoringAntMatchers("/api/login", "/api/signup", "api/report")
+                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()).and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+                .exceptionHandling().authenticationEntryPoint(restAuthenticationEntryPoint).and()
+                .addFilterBefore(jwtAuthenticationTokenFilter(), BasicAuthenticationFilter.class)
+                .authorizeRequests().anyRequest().authenticated().and().formLogin().loginPage("/api/login")
+                .successHandler(authenticationSuccessHandler).failureHandler(authenticationFailureHandler)
+                .and().logout().logoutRequestMatcher(new AntPathRequestMatcher("/api/logout"))
+                .logoutSuccessHandler(logoutSuccess).deleteCookies(TOKEN_COOKIE);
 
-  }
+    }
 
 }
