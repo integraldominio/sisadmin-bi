5a8515cd19ffe5a07ddbb438710d37e37b22bb29
lyndontavares <integraldominio@gmail.com>
2018-07-03 14:47:36 -0300

    Movido para https://github.com/integraldominio/ng-estudos.git

--- estudos/api_express/config/stripe.js
+++
@@ -1,4 +0,0 @@
-const config = require('./main');
-const stripe = require('stripe')(config.stripeApiKey);
-
-stripe.setTimeout(20000);
--- estudos/file_upload_mongoose/client.js
+++
@@ -1,8 +0,0 @@
-const axios = require('axios')
-
-function uploadImage () {
-  const files = document.getElementById('INPUT_TAG').files
-  const formData = new FormData()
-  formData.append('image', files[0])
-  axios.post('YOUR_URL/api/image', formData)
-}
